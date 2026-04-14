fdb2fa6b626242c29abfc680098d2488551244cc
brunoburthf <brunoburthf@gmail.com>
2026-04-14 15:01:28 -0300

    fix: local.js remove class; spread grid display

--- js/local.js
+++ js/local.js
@@ -1,4 +1,4 @@
 // Exibe elementos .val-restrito apenas em localhost
 if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
-  document.querySelectorAll('.val-restrito').forEach(el => el.style.display = '');
+  document.querySelectorAll('.val-restrito').forEach(el => el.classList.remove('val-restrito'));
 }
